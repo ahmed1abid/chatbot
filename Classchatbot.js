7e43d7e68aa2a650fbe901e49f2784c470e0f20c
ahmed <aabid@enssat.fr>
2023-04-14 11:36:34 -0400

    we have the interface

--- Classchatbot.js
+++ Classchatbot.js
@@ -1,30 +1,40 @@
-class ChatBot {
+class Classchatbot {
+  
     constructor(name, personality, interfaces) {
-      this.name = name;
-      this.personality = personality;
-      this.interfaces = interfaces;
-    }
-  
-    addInterface(interface) {
-      this.interfaces.push(interface);
-    }
-  
-    removeInterface(interface) {
-      const index = this.interfaces.indexOf(interface);
-      if (index > -1) {
-        this.interfaces.splice(index, 1);
-      }
-    }
-  
-    addPersonality(personality) {
-      this.personality.push(personality);
-    }
-  
-    removePersonality(personality) {
-      const index = this.personality.indexOf(personality);
-      if (index > -1) {
-        this.personality.splice(index, 1);
-      }
+    this.name = name;
+    this.personality = personality;
+    this.interfaces = interfaces;
+    this.chatlog = [];
+  }
+
+  addInterface(interfaces) {
+    this.interfaces.push(interfaces);
+  }
+
+  removeInterface(interfaces) {
+    const index = this.interfaces.indexOf(interfaces);
+    if (index > -1) {
+      this.interfaces.splice(index, 1);
     }
   }
-  
+
+  addPersonality(personality) {
+    this.personality.push(personality);
+  }
+
+  removePersonality(personality) {
+    const index = this.personality.indexOf(personality);
+    if (index > -1) {
+      this.personality.splice(index, 1);
+    }
+  }
+
+  addToChatLog(message, isUser) {
+    this.chatlog.push({ message: message, isUser: isUser });
+  }
+
+  getChatLog() {
+    return this.chatlog;
+  }
+}
+module.exports = Classchatbot;
